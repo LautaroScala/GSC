4bc59a60a9001629e87946622118f6be862ac249
LautaroScala <scala.lautaro@hotmail.com>
2022-11-13 17:08:04 -0300

    Clases

---
+++ typescript/src/Clases.ts
@@ -0,0 +1,29 @@
+export class Vehiculo{
+
+    // propiedades publicas
+    
+    // readonly: no se puede modificar la propiedad
+
+    constructor(private nombre:string,private precio:number,private fabricante:string){
+    }
+
+    greet(){
+        console.log(`El vehiculo ${this.nombre} cuesta ${this.precio}`);
+    }
+    set _fabricante(fabricante:string){
+        this.fabricante = fabricante;
+    }
+    get _fabricante(){
+        return this.fabricante
+    }
+}
+
+export class Moto extends Vehiculo{
+    constructor(nombre:string,precio:number, _fabricante:string, private importador:string){
+        super(nombre,precio,_fabricante);
+        this.importador = importador;
+    }
+    get _importador(){
+        return this.importador;
+    }
+}
---
+++ typescript/src/Clases2.ts
@@ -0,0 +1,7 @@
+import { Vehiculo, Moto } from "./Clases";
+
+
+let moto: Moto = new Moto('X3',500000,'BMW', 'Desconocido');
+
+console.log(moto._importador);
+moto.greet();
