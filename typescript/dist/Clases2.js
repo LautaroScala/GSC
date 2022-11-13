1d21fc02f4004275b9000f2c4ec1e55e35077083
LautaroScala <scala.lautaro@hotmail.com>
2022-11-13 17:06:56 -0300

    Creada carpeta -Ejercicios resueltos-

---
+++ typescript/dist/Clases2.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Clases_1 = require("./Clases");
+let moto = new Clases_1.Moto('X3', 500000, 'BMW', 'Desconocido');
+console.log(moto._importador);
+moto.greet();
