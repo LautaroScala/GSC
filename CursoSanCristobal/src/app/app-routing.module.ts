dea98b618c88336dbf712637a0ff4a0e4f8e13b0
LautaroScala <scala.lautaro@hotmail.com>
2022-11-17 02:24:22 -0300

    reestructurada la app

--- CursoSanCristobal/src/app/app-routing.module.ts
+++ CursoSanCristobal/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DosComponent } from './dos/dos.component';
+import { HijoComponent } from './hijo/hijo.component';
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
 import { UnoComponent } from './uno/uno.component';
 
 const routes: Routes = [
-  { path : 'uno', component: UnoComponent, children:[
-    {path: 'test/:id',component:TestComponent}
-  ] },
+  { path : 'uno', component: UnoComponent},
+  { path: 'test/:id',component:TestComponent},
   { path : 'dos', component: DosComponent, data : { title: 'Componente dos' } },
+  { path: 'tres', component:HijoComponent},
   { path:'',component: HomeComponent}
 ];
 
